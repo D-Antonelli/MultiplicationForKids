fa75dded30d0e1ee5449abdf1af3a8e7a139e885
derya <derya@boonandmoil.dev>
2022-03-07 10:38:01 +0000

    Initial Commit

---
+++ MultiplicationForKids/MultiplicationForKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationForKidsApp.swift
+//  MultiplicationForKids
+//
+//  Created by Derya Antonelli on 07/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationForKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
