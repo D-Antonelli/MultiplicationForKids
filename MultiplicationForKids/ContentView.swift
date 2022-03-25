16eaa3fe4750e00befdc7fbbc767b9cb768d5694
derya <derya@boonandmoil.dev>
2022-03-25 14:10:02 +0300

    instantiates observable object

--- MultiplicationForKids/ContentView.swift
+++ MultiplicationForKids/ContentView.swift
@@ -43,6 +43,8 @@ struct ContentView: View {
     @State private var numberOfQuestions = 0
     @State private var showSecondView = false
     
+    @StateObject var questions = Questions()
+    
     public var canStartGame: Bool {
         return table > 0 && numberOfQuestions > 0
     }
@@ -109,6 +111,23 @@ struct ContentView: View {
             
         }
 
+    }
+    
+    func populateQuestions() {
+        let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        var times = numbers
+        
+        for i in 1...numberOfQuestions {
+         if(times.count < 1) {
+             times = numbers
+            }
+        let randomIndex = Int.random(in: 0..<times.count)
+        let randomTime = times[randomIndex]
+            
+            questions.questions.append(Question(table: table, time: randomTime, index: i - 1))
+        times.remove(at: randomIndex)
+        }
+        
     }
     
     func selectTable(_ number: Int) {
