415d4bd00d2721ec21dc055b7e34f626be569345
derya <derya@boonandmoil.dev>
2022-03-25 15:45:38 +0300

    creates observable data object for questions

--- MultiplicationForKids/ContentView.swift
+++ MultiplicationForKids/ContentView.swift
@@ -32,12 +32,17 @@ struct Question: Identifiable, Hashable, View {
     }
 }
 
+class Questions: ObservableObject {
+    @Published var questions = [Question]()
+}
 
 
 struct ContentView: View {
     @State private var table = 0
     @State private var numberOfQuestions = 0
     @State private var showSecondView = false
+    
+    @StateObject var questions = Questions()
     
     public var canStartGame: Bool {
         return table > 0 && numberOfQuestions > 0
@@ -85,6 +90,7 @@ struct ContentView: View {
             
             Section {
                 Button("Start") {
+                    populateQuestions()
                     showSecondView.toggle()
                 }
                 .disabled(canStartGame == false)
@@ -101,10 +107,27 @@ struct ContentView: View {
             Spacer()
         }
         .sheet(isPresented: $showSecondView) {
-            SecondView(table: table, numberOfQuestions: numberOfQuestions)
+            SecondView(numberOfQuestions: numberOfQuestions)
             
         }
+        .environmentObject(questions)
 
+    }
+    
+    func populateQuestions() {
+        let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        var times = numbers
+        
+        for i in 1...numberOfQuestions {
+         if(times.count < 1) {
+             times = numbers
+            }
+        let randomIndex = Int.random(in: 0..<times.count)
+        let randomTime = times[randomIndex]
+            
+            questions.questions.append(Question(table: table, time: randomTime, index: i - 1))
+        times.remove(at: randomIndex)
+        }
     }
 
     
--- MultiplicationForKids/SecondView.swift
+++ MultiplicationForKids/SecondView.swift
@@ -8,22 +8,21 @@
 import SwiftUI
 
 struct SecondView: View {
-    public var table = 0
     public var numberOfQuestions = 0
     @State private var correctAnswers = 0
     @State private var answers: [Int]
-    @State private var questions = [Question]()
     @State private var showThirdView = false
     
-    init(table: Int, numberOfQuestions: Int) {
-        self.table = table
+    @EnvironmentObject var questions: Questions
+    
+    init(numberOfQuestions: Int) {
         self.numberOfQuestions = numberOfQuestions
         self.answers = Array(repeating: 0, count: self.numberOfQuestions)
     }
 
     var body: some View {
         VStack {
-            List(questions) { question in
+            List(questions.questions) { question in
                 HStack {
                     question
                     TextField("answer", value: $answers[question.index], format: .number)
@@ -39,35 +38,17 @@ struct SecondView: View {
                 submitAnswers()
             }
             
+            
         }
-        .onAppear {
-            populateQuestions()
-        }
+        
         .sheet(isPresented: $showThirdView) {
             ThirdView(correct: correctAnswers, total: numberOfQuestions)
         }
     }
         
     
-    func populateQuestions() {
-        let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-        var times = numbers
-        
-        for i in 1...numberOfQuestions {
-         if(times.count < 1) {
-             times = numbers
-            }
-        let randomIndex = Int.random(in: 0..<times.count)
-        let randomTime = times[randomIndex]
-            
-        questions.append(Question(table: table, time: randomTime, index: i - 1))
-        times.remove(at: randomIndex)
-        }
-        
-    }
-    
     func submitAnswers() {
-        questions.forEach() { question in
+        questions.questions.forEach() { question in
             let result = question.time * question.table
             let answer = answers[question.index]
             if(answer == result) {
@@ -80,10 +61,9 @@ struct SecondView: View {
 }
 
 struct SecondView_Previews: PreviewProvider {
-    static let tablePreview = 1
     static let numberOfQuestionsPreview = 5
     
     static var previews: some View {
-        SecondView(table: tablePreview, numberOfQuestions: numberOfQuestionsPreview)
+        SecondView(numberOfQuestions: numberOfQuestionsPreview)
     }
 }
