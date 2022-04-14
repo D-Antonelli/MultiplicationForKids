340dd0cbd1259a3f8aea560fb3cabb758856418d
derya <derya@boonandmoil.dev>
2022-04-14 08:47:25 +0100

    refactors code

--- MultiplicationForKids/ContentView.swift
+++ MultiplicationForKids/ContentView.swift
@@ -8,29 +8,6 @@
 import SwiftUI
 
 //https://coolors.co/palette/ff595e-ffca3a-8ac926-1982c4-6a4c93
-    
-
-struct Page: View {
-    public var button: TableSelectionButton
-    public var title: String
-    public var subtitle: String
-    
-    var body: some View {
-        VStack {
-            button
-            
-            Text(title)
-                .foregroundColor(Color(red: 1, green: 0.349, blue: 0.3686))
-                .font(.titleFont)
-            Text(subtitle)
-                .foregroundColor(.white)
-            Spacer()
-        }
-        
-    }
-}
-
-
 
 struct ContentView: View {
     @State private var showSecondView = false
---
+++ MultiplicationForKids/Page.swift
@@ -0,0 +1,36 @@
+//
+//  Page.swift
+//  MultiplicationForKids
+//
+//  Created by Derya Antonelli on 14/04/2022.
+//
+
+import SwiftUI
+
+struct Page: View {
+    public var button: TableSelectionButton
+    public var title: String
+    public var subtitle: String
+    
+    var body: some View {
+        VStack {
+            button
+            
+            Text(title)
+                .foregroundColor(Color(red: 1, green: 0.349, blue: 0.3686))
+                .font(.titleFont)
+            Text(subtitle)
+                .foregroundColor(.white)
+            Spacer()
+        }
+        
+    }
+}
+
+
+struct Page_Previews: PreviewProvider {
+    static let selectPreview: (Int) -> Void = { _ in }
+    static var previews: some View {
+        Page(button: TableSelectionButton(image: "panda", table: 2, select: selectPreview), title: "title1", subtitle: "title2")
+    }
+}
